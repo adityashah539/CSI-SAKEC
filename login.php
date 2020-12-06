66aef7b13046cdf3022ea9d13ce9a525f58100a8
adityashah539 <55307622+adityashah539@users.noreply.github.com>
2020-12-06 15:14:49 +0530

    v1.5

--- login.php
+++ login.php
@@ -1,79 +1,41 @@
 <?php
-//This script will handle login
-session_start();
-// check if the user is already logged in
-if(isset($_SESSION['username']))
-{
+if (isset($_SESSION['username'])) {
     header("location: index.html");
     exit;
 }
 require_once "config.php";
 $username = $password = "";
 $err = "";
-// if request method is post
-if ($_SERVER['REQUEST_METHOD'] == "POST")
-{
-    $trimun=trim($_POST['user']);
-    $trimpass=trim($_POST['password']);
-    if(empty($trimun) || empty($trimpass))
-    {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (empty(trim($_POST['email'])) || empty(trim($_POST['password']))) {
         $err = "Please enter username + password";
-    }
-    else
-    {
-        $username = trim($_POST['user']);
+    } else {
+        $username = trim($_POST['email']);
         $password = trim($_POST['password']);
     }
-    if(empty($err))
-    {
-        $sql = "SELECT EmailId, Password  FROM data WHERE EmailId = ?";
+    if (empty($err)) {
+        $sql = "SELECT emailID, password  FROM userdata WHERE emailID = ?";
         $stmt = mysqli_prepare($conn, $sql);
         mysqli_stmt_bind_param($stmt, "s", $param_username);
-        $param_username = $username;        
+        $param_username = $username;
         // Try to execute this statement
-        if(mysqli_stmt_execute($stmt))
-        {
+        if (mysqli_stmt_execute($stmt)) {
             mysqli_stmt_store_result($stmt);
-            if(mysqli_stmt_num_rows($stmt) == 1)
-            {
+            if (mysqli_stmt_num_rows($stmt) == 1) {
                 mysqli_stmt_bind_result($stmt, $username, $hashed_Password);
-                if(mysqli_stmt_fetch($stmt))
-                {
-                    echo $hashed_Password." ".$password;
-                    if($password===$hashed_Password)
-                    {
+                if (mysqli_stmt_fetch($stmt)) {
+                    //echo $hashed_Password . " " . $password;
+                    if ($password === $hashed_Password) {
                         // this means the password is corrct. Allow user to login
                         session_start();
                         $_SESSION["username"] = $username;
-                        $_SESSION["id"] = $id;
-                        $_SESSION["loggedin"] = true; 
-                        //Redirect user to welcome page
-                        echo "Login done";
-                        header("location: index.html");        
+                        $_SESSION["loggedin"] = true;
+                        //echo $hashed_Password . " " . $password;
+                        header("location: index.html");
                     }
-                    else 
-                    {
-                        echo "password worng ";
-                    }
+                    else{echo'password worng';}
                 }
-                else
-                {
-
-                }
-            }
-            else 
-            {
-                echo " ";
-            }
-        }
-        else 
-        {
-
-        }
-    }
-    else
-    {
-
-    }  
+            } 
+        } 
+    }else{echo'empty';} 
 }
-?>
---
+++ signup.php
@@ -0,0 +1,33 @@
+<?php
+require_once "config.php";
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $sql = "SELECT * FROM userdata WHERE emailId = ? or phonenumber= ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    if ($stmt) {
+        $param_email = trim($_POST["email"]);
+        $param_phonenumber = trim($_POST["phonenumber"]);
+        mysqli_stmt_bind_param($stmt, "si", $param_email,$param_phonenumber);
+        if (mysqli_stmt_execute($stmt)) {
+            mysqli_stmt_store_result($stmt);
+            if (mysqli_stmt_num_rows($stmt) == 1) {
+                echo'email or phone  has been taken';
+            } else {
+                $email = trim($_POST["email"]);
+                $password = trim($_POST["password"]);
+                $phonenumber = trim($_POST["phonenumber"]);
+                $confrimpassword = trim($_POST["confrimpassword"]);
+                if ($password === $confrimpassword) {
+                    $branch = trim($_POST["branch"]);
+                    $class = trim($_POST["year"]);
+                    $firstname = trim($_POST["firstname"]);
+                    $lastname = trim($_POST["lastname"]);
+                    $sql = "INSERT INTO userdata ( firstName, lastName, emailID, phonenumber, branch, class, password,role) VALUES ('$firstname','$lastname','$email','$phonenumber','$branch','$class','$password','s')";
+                    $stmt = mysqli_query($conn, $sql);
+                    header("location: login.html");
+                    mysqli_close($conn);
+                }
+            }
+        }
+    }
+} 
+?>
